6e59415a14f229adbf5c83bff31197822ecfc143
kuan1701 <kuanchin17011993@gmail.com>
2021-06-13 19:48:20 +0300

    initial commit

--- src/main/java/com/example/webproject/pms/controller/GreetingController.java
+++ src/main/java/com/example/webproject/pms/controller/GreetingController.java
@@ -17,16 +17,12 @@ public class GreetingController {
 	@Autowired
 	private MessageRepository messageRepository;
 	
-	@GetMapping("/greeting")
-	public String greeting(
-			@RequestParam(name = "name", required = false, defaultValue = "World") String name,
-			Map<String, Object> model
-	) {
-		model.put("name", name);
+	@GetMapping("/")
+	public String greeting() {
 		return "greeting";
 	}
 	
-	@GetMapping
+	@GetMapping("/main")
 	public String main(Map<String, Object> model) {
 		
 		Iterable<Message> messages = messageRepository.findAll();
@@ -34,7 +30,7 @@ public class GreetingController {
 		return "main";
 	}
 	
-	@PostMapping
+	@PostMapping("/main")
 	public String add(@RequestParam String text,
 					  @RequestParam String tag,
 					  Map<String, Object> model) {
