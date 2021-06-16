1791774190e4141288f5d60edb7afc73fe549ef9
kuan1701 <kuanchin17011993@gmail.com>
2021-06-16 14:41:35 +0300

    initial commit

---
+++ src/main/java/com/example/webproject/pms/controller/MainController.java
@@ -0,0 +1,57 @@
+package com.example.webproject.pms.controller;
+
+import com.example.webproject.pms.model.Message;
+import com.example.webproject.pms.repository.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class MainController {
+	
+	@Autowired
+	private MessageRepository messageRepository;
+	
+	@GetMapping("/")
+	public String greeting() {
+		return "greeting";
+	}
+	
+	@GetMapping("/main")
+	public String main(Map<String, Object> model) {
+		
+		Iterable<Message> messages = messageRepository.findAll();
+		model.put("messages", messages);
+		return "main";
+	}
+	
+	@PostMapping("/main")
+	public String add(@RequestParam String text,
+					  @RequestParam String tag,
+					  Map<String, Object> model) {
+		Message message = new Message(text, tag);
+		messageRepository.save(message);
+		
+		Iterable<Message> messages = messageRepository.findAll();
+		model.put("messages", messages);
+		return "main";
+	}
+	
+	@PostMapping("filter")
+	public String filter(@RequestParam String filter,
+						 Map<String, Object> model) {
+		Iterable<Message> messageList;
+		if (filter!= null && !filter.isEmpty()) {
+			messageList = messageRepository.findByTag(filter);
+		} else {
+			messageList = messageRepository.findAll();
+		}
+		model.put("messages", messageList);
+		return "main";
+	}
+}
---
+++ src/main/java/com/example/webproject/pms/model/Role.java
@@ -0,0 +1,5 @@
+package com.example.webproject.pms.model;
+
+public enum Role {
+	USER
+}
